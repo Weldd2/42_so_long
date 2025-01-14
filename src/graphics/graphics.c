0eabff098709c79f43e5ee12e4bfc67a0e0c37dc
Your Name <a.mura27400@gmail.com>
2025-01-14 20:03:16 +0100

    add mgc to mlx

--- src/graphics/graphics.c
+++ src/graphics/graphics.c
@@ -6,7 +6,7 @@
 /*   By: antoinemura <antoinemura@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/13 23:58:20 by antoinemura       #+#    #+#             */
-/*   Updated: 2025/01/14 19:58:34 by antoinemura      ###   ########.fr       */
+/*   Updated: 2025/01/14 20:02:08 by antoinemura      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ void	graph(t_map map)
 	mlx_t	*mlx;
 
 	mlx = mlx_init(map.width * 32, map.height * 32, "42Balls", true);
+	mem_mgc_add_block((void *)mlx, (void *)(void *)mlx_terminate);
 	if (!mlx)
 		ft_error();
 	images_to_window(mlx, map);
