701e19fdeeda3f05548bdec98fb9d09844d2a6ce
Your Name <a.mura27400@gmail.com>
2025-01-21 15:34:53 +0100

    norm fix

--- src/graphics/input/input.c
+++ src/graphics/input/input.c
@@ -6,13 +6,13 @@
 /*   By: antoinemura <antoinemura@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/14 22:07:55 by antoinemura       #+#    #+#             */
-/*   Updated: 2025/01/21 15:08:35 by antoinemura      ###   ########.fr       */
+/*   Updated: 2025/01/21 15:33:42 by antoinemura      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-t_ok	handle_collision(mlx_key_data_t keydata, t_map *map)
+static t_ok	handle_collision(mlx_key_data_t keydata, t_map *map)
 {
 	if (keydata.key == MLX_KEY_W)
 		return (map->tiles[map->p_y - 1][map->p_x] == '1');
@@ -25,7 +25,7 @@ t_ok	handle_collision(mlx_key_data_t keydata, t_map *map)
 	return (E_ERR);
 }
 
-void	handle_move(mlx_key_data_t keydata, t_map *map, t_images *images)
+static void	handle_move(mlx_key_data_t keydata, t_map *map, t_images *images)
 {
 	if (keydata.key == MLX_KEY_W)
 	{
@@ -49,9 +49,9 @@ void	handle_move(mlx_key_data_t keydata, t_map *map, t_images *images)
 	}
 }
 
-void	handle_collect(t_map *map, t_images *images)
+static void	handle_collect(t_map *map, t_images *images)
 {
-	size_t			i;
+	size_t	i;
 
 	i = 0;
 	if (map->tiles[map->p_y][map->p_x] == 'C')
@@ -59,8 +59,8 @@ void	handle_collect(t_map *map, t_images *images)
 		map->c_count--;
 		while (i < images->blocC->count)
 		{
-			if ((size_t)images->blocC->instances[i].x == map->p_x * 32 &&
-				(size_t)images->blocC->instances[i].y == map->p_y * 32)
+			if ((size_t)images->blocC->instances[i].x == map->p_x * 32
+				&& (size_t)images->blocC->instances[i].y == map->p_y * 32)
 			{
 				images->blocC->instances[i].z--;
 				map->tiles[map->p_y][map->p_x] = '0';
@@ -70,19 +70,20 @@ void	handle_collect(t_map *map, t_images *images)
 	}
 }
 
-void	handle_escape(t_map *map, mlx_t *mlx)
+static void	handle_escape(t_map *map, mlx_t *mlx)
 {
 	if (map->tiles[map->p_y][map->p_x] == 'E' && map->c_count == 0)
 		mlx_close_window(mlx);
 }
 
-void	direction_keyhook(mlx_key_data_t keydata, void* param)
+static void	direction_keyhook(mlx_key_data_t keydata, void *param)
 {
 	t_game			*game;
 	static size_t	count = 0;
 
 	game = (t_game *)param;
-	if (keydata.action == MLX_PRESS && handle_collision(keydata, game->map) == E_OK)
+	if (keydata.action == MLX_PRESS
+		&& handle_collision(keydata, game->map) == E_OK)
 	{
 		handle_move(keydata, game->map, game->images);
 		handle_collect(game->map, game->images);
@@ -91,4 +92,3 @@ void	direction_keyhook(mlx_key_data_t keydata, void* param)
 		handle_escape(game->map, game->mlx);
 	}
 }
-	
